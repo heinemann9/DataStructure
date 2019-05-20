abc1d07357bee9116147dba1a612558bcb71c61f
ksa <sinankng@gmail.com>
2019-05-20 21:02:28 +0900

    modified for LinkedList.

---
+++ LinkedList/LinkedList.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef struct LinkedList
+{
+    string item;
+    LinkedList *next;
+
+    LinkedList(string item)
+    {
+        this->item = item;
+        this->next = nullptr;
+    }
+}LinkedList;
+
+int main(void)
+{
+    LinkedList *header = nullptr;
+    string item[] = {"12345", "2345", "345", "45"};
+
+    cout<<"sizeof(item):"<<sizeof(item)<<endl;
+    cout<<"sizeof(item[0]):"<<sizeof(item[0])<<endl;
+
+    int i;
+    for (i = 0; i < sizeof(item)/sizeof(item[0]); ++i)
+    {
+        LinkedList *linkedList = new LinkedList(item[i]);
+
+        linkedList->next = header;
+        header = linkedList;
+    }
+
+    LinkedList *temp = header;
+    for (i = 0; i < sizeof(item)/sizeof(item[0]); ++i)
+    {
+        cout<<"item: "<<temp->item<<endl;
+        temp = temp->next;
+    }
+
+    return 0;
+}
